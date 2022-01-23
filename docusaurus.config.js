7d66f26ab2488c4626e6bc8e3ea3e2fdb50994b0
ekidneyrh <ekidney@redhat.com>
2022-01-23 16:07:33 +0000

    adding logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
         title: 'Web Accessibility Handbook',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/WAD.png',
         },
         items: [
           {
