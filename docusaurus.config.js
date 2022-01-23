f575fad0db81ea4b3754a08c6f06a626cf3dbeb6
ekidneyrh <ekidney@redhat.com>
2022-01-23 13:45:36 +0000

    editing landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Documentation,
                 to: '/docs/intro',
               },
             ],
